ff10e8e0f43f5f5547fe13b4b67bdc7c0cbd27b1
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2019-08-13 19:12:00 +0300

    add SyncWrapper class for more user friendly syncing workflow

---
+++ app/scripts/SyncWrapper.php
@@ -0,0 +1,46 @@
+<?php
+    class SyncWrapper
+    {
+        function __construct(string $type)
+        {
+            $key = array(
+                'stops' => new \App\scripts\StopLoader2()
+            );
+            foreach($key as $name => $loader)
+            {
+                if($name == $type || $type == 'all')
+                {
+                    $this->fast_sync($loader);
+                }
+            }
+        }
+        private function fast_sync($loader)
+        {
+            $syncer = new \App\scripts\Syncer($loader);
+            $syncer->load();
+
+            $syncer->check_diff();
+
+            echo("\nTo be added:\n\n");
+            foreach($syncer->to_add as $item)
+            {
+                echo($item->stringify() . "\n");
+            }
+
+            echo("\nTo be deleted:\n\n");
+            foreach($syncer->to_delete as $item)
+            {
+                echo($item->stringify() . "\n");
+            }
+
+            echo("\nTo be added: "  .count($syncer->to_add) . "\n");
+            echo("\nTo be deleted: " .count($syncer->to_delete) . "\n");
+
+            $answer = readline("Apply changes? (y/n) ");
+            if($answer == "y")
+            {
+                $syncer->apply();
+            }
+        }
+    }
+?>
