658fca0fc83730e93bb50dc44e2b04780c0f79d2
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2019-08-15 21:27:18 +0300

    Add departures table migration

---
+++ database/migrations/2019_08_15_180923_create_departures_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeparturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departures', function (Blueprint $table) {
+            $table->date('date')->index();
+            $table->bigInteger('route_stop_id')->index();
+            $table->time('expected_time');
+            $table->time('actual_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departures');
+    }
+}
