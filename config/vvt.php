6c07ada1797c8d1eee72feec79d4c039cf275a4f
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2019-08-16 12:45:07 +0300

    Move some traffi and loging configs to vvt config file

--- config/vvt.php
+++ config/vvt.php
@@ -1,7 +1,24 @@
 <?php
     return [
 
-	'transport_subtypes' => ['Greitasis autobusas', 'Autobusas', 'Troleibusas'],
+	    'transport_subtypes' => [
+            'Greitasis autobusas',
+            'Autobusas',
+            'Troleibusas',
+        ],
 
-    ];
+        'transport_types' => [
+            'trolleybus',
+            'bus',
+        ],
+
+        'trafi_api_url' => 'https://www.trafi.com/api/schedules/vilnius/',
+
+        'valid_tracks' => [
+            'a-b',
+            'b-a',
+        ],
+
+        'log' => true,
+    ]; 
 ?>
