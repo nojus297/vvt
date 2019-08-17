cfcfaccf549c526a04b5386817dde41299f1f8e7
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2019-08-17 18:58:00 +0300

    Add transport types conversion to config and load trolleybus only

--- config/vvt.php
+++ config/vvt.php
@@ -2,14 +2,21 @@
     return [
 
 	    'transport_subtypes' => [
-            'Greitasis autobusas',
-            'Autobusas',
+            //'Greitasis autobusas',
+            //'Autobusas',
             'Troleibusas',
         ],
 
         'transport_types' => [
             'trolleybus',
-            'bus',
+            //'bus',
+        ],
+
+        'types_conversion' =>[
+            'trolleybus' => 1,
+            'bus' => 2,
+            1 => 'trolleybus',
+            2 => 'bus',
         ],
 
         'trafi_api_url' => 'https://www.trafi.com/api/schedules/vilnius/',
