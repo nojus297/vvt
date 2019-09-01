5946beff065d356677997c197ff5bc505e6fd006
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2019-09-01 19:19:43 +0300

    Add get_trafi_trafic_url

--- app/scripts/functions.php
+++ app/scripts/functions.php
@@ -1,6 +1,5 @@
 <?php
     namespace App\scripts;
-
     function info($text)
     {
         echo($text . "\n");
@@ -16,5 +15,11 @@
         $format = "https://www.trafi.com/api/times/vilnius/";
         $format .= "scheduled?scheduleId=%s&trackId=a-b&stopId=%s";
         return sprintf($format, $route_id, $stop_id);
+    }
+    function get_trafi_trafic_url($route_id, $dir)
+    {
+        $url = "https://www.trafi.com/api/realtime-vehicles/vilnius?";
+        $url .= "scheduleId={$route_id}&trackId={$dir}";
+        return $url;
     }
 ?>
