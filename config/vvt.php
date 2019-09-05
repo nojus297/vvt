3deaa513074eaafa8d8b30c924e1fdab8705ee89
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2019-09-05 21:45:22 +0300

    Add min degree calc range to config file

--- config/vvt.php
+++ config/vvt.php
@@ -12,12 +12,14 @@
             //'bus',
         ],
 
-        'types_conversion' =>[
+        'types_conversion' => [
             'trolleybus' => 1,
             'bus' => 2,
             1 => 'trolleybus',
             2 => 'bus',
         ],
+
+        'min_degree_calc_range' => 10,
 
         'trafi_api_url' => 'https://www.trafi.com/api/schedules/vilnius/',
 
