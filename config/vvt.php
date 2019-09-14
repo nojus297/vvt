588b498f27fd784240c30efaa4be7749c92c5eb8
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2019-09-14 19:57:22 +0300

    Add min_track_range to config file

--- config/vvt.php
+++ config/vvt.php
@@ -21,7 +21,11 @@
 
         'stops.lt_vehicles_url' => 'https://stops.lt/vilnius/gps.txt',
 
+        'track_up_to' => (new DateTime())->setTime(23, 55),
+
         'min_degree_calc_range' => 10,
+
+        'min_track_range' => 25,
 
         'trafi_api_url' => 'https://www.trafi.com/api/schedules/vilnius/',
 
