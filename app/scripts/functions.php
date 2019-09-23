b79096d6f8ed143ba98191e82b3465b209c68337
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2019-09-23 20:51:28 +0300

    Add normalize_angle function in function file

--- app/scripts/functions.php
+++ app/scripts/functions.php
@@ -42,4 +42,13 @@
     {
         return new \DateTime('now', new \DateTimeZone('Europe/Vilnius'));
     }
+    function normalize_angle($angle)
+    {
+        $angle = ($angle % 360 + 360) % 360;
+        if($angle > 180)
+        {
+            $angle -= 360;
+        }
+        return $angle;
+    }
 ?>
