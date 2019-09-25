c9c2ddbdb63bf31f47925a2e0c701345b18a7489
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2019-09-25 22:08:19 +0300

    Add more config variables to vvt.config

--- config/vvt.php
+++ config/vvt.php
@@ -21,13 +21,19 @@
 
         'stops_lt_vehicles_url' => 'https://stops.lt/vilnius/gps.txt',
 
-        'track_up_to' => \App\scripts\Now()->setTime(23, 55),
+        'track_up_to' => \App\scripts\Now()->setTime(20, 35),
 
         'min_degree_calc_range' => 10,
 
         'min_track_range' => 80,
 
         'max_track_degree_diff' => 40,
+
+        'min_track_time_arr' => [4, 30],
+
+        'max_track_time_arr' => [23, 59, 50],
+
+        'max_too_early_offset' => new DateInterval('PT5M'),
 
         'trafi_api_url' => 'https://www.trafi.com/api/schedules/vilnius/',
 
