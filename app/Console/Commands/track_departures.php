4342d21dead927f8f5c87f5335cda3f2ce71aa47
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2019-09-29 00:16:21 +0300

    Add cli arguments to vvt:track_departures command

--- app/Console/Commands/track_departures.php
+++ app/Console/Commands/track_departures.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use stdClass;
 
 class track_departures extends Command
 {
@@ -11,14 +12,18 @@ class track_departures extends Command
      *
      * @var string
      */
-    protected $signature = 'vvt:track_departures';
+    protected $signature = "vvt:track_departures
+                            {--l|load-expected : Load today's departures}
+                            {--p|push-data : Push analyzed data to database}
+                            {--t|time= : Time to track up to}
+                            {--r|return : Return processed data}";
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Track vehicle departures';
+    protected $description = 'Track, analyze and push vehicle departures';
 
     /**
      * Create a new command instance.
@@ -37,10 +42,31 @@ class track_departures extends Command
      */
     public function handle()
     {
+        if($this->option('load-expected'))
+        {
+            $loader = new \App\scripts\DeparturesLoader;
+            $loader->load();
+        }
+        $time = $this->option('time');
+        if($time)
+        {
+            $time = \App\scripts\lt_time($time);
+        }
         $tracker = new \App\scripts\DeparturesTracker;
-        $result  = $tracker->get_actual_departures();
+        $result  = $tracker->get_actual_departures($time);
         $analyzer = new \App\scripts\DeparturesAnalyzer();
         $analyzer->analyze($result);
-        // $analyzer->push();
+
+        if($this->option('push-data'))
+        {
+            $analyzer->push();
+        }
+        if($this->option('return'))
+        {
+            $output = new stdClass;
+            $output->actual_departures = $result->departures;
+            $output->analyzed = $analyzer->departures;
+            return $output;
+        }
     }
 }
